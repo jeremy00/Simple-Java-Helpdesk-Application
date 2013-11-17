c9af613ba1219a3a856af5b6b110ddf0f8cd89ae
Jeremy May <jwmauynoh@gmail.com>
2013-11-16 21:33:44 -0500

    Added database
    Add/delete ticket to database(class) ArrayList

---
+++ Daily/src/database.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author w032jwm
+ */
+
+import java.util.ArrayList;
+public class database {
+    ArrayList tickets = new ArrayList();
+    
+    public void addTicket(phonecallTicket ticket){
+    tickets.add(ticket);
+    }
+    
+    public void removeTicket(int num){
+    if (!tickets.isEmpty() && num < tickets.size() && num > 0){
+       tickets.remove(num);
+    }
+    }//end removeTicket
+    
+}
---
+++ Daily/src/phonecallTicket.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mayj
+ */
+public class phonecallTicket {
+    
+String who;
+String phone;
+Double tag;
+String problem;
+String notes;
+
+
+}
